8124fd03e849e73288035fcfe46d229f5d007352
ben-abdurrehman <abdurrehman12k6@gmail.com>
2025-06-19 10:01:38 +0500

    hero section completed

---
+++ src/components/ui/HowWeWork.js
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+
+import values from "@/data/objects/Values";
+// import Grains from "@/assets/Grains.png";
+import Blob from "@/assets/FloatingAssets/Blob.png";
+
+export default function HowWeWork() {
+
+  return (
+    <section
+      id="about"
+      className="font-roboto bg-gradient-to-bl from-black via-slate-950 to-slate-950 w-full relative py-28  overflow-hidden"
+    >
+      <div className="max-w-6xl mx-auto px-4 md:px-6 lg:px-8 relative z-10">
+        <h2 className="text-stroke font-poppins text-2xl md:text-5xl 2xl:text-7xl mb-10 font-bold text-center text-white">
+          Who we are BTW?
+        </h2>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-5 w-[90%] md:w-4/5 lg:w-4xl xl:w-5xl 2xl:w-7xl mx-auto relative z-10">
+        {values.map((value, index) => (
+          <div
+            key={index}
+            className={`${index === 0 || index === 3 ? "col-span-1" : (index === 1 || index === 2 ? "col-span-2" : "")}  flex flex-col border border-gray-400 bg-transparent backdrop-blur-3xl items-start space-y-3 p-7 rounded-xl`}
+          >
+            {/* <Image src={value.icon} alt={value.title} className="w-12 h-12" /> */}
+            <h3 className="text-lg font-bold text-white">{value.title}</h3>
+            <p className="text-gray-400">{value.description}</p>
+          </div>
+        ))}
+      </div>
+
+      {/* <Image
+        src={Grains}
+        alt="Grains"
+        className="absolute inset-0 object-cover -z-40 w-full h-full opacity-40"
+      /> */}
+      <Image
+        src={Blob}
+        alt="Blob"
+        className="absolute inset-0 object-cover -z-30 w-full h-full opacity-40"
+      />
+    </section>
+  );
+}
---
+++ src/data/objects/Values.js
@@ -0,0 +1,29 @@
+import Team from "@/assets/ValueIcons/Team.png";
+import Heart from "@/assets/ValueIcons/Heart.png";
+import Forward from "@/assets/ValueIcons/Forward.png";
+import SmileEmogi from "@/assets/ValueIcons/SmileEmogi.png";
+
+const values = [
+    {
+      title: "A Team That Cares",
+      description: "People matter. Listening, understanding, and providing the right support create an environment where everyone thrives and delivers their best work.",
+      icon: Team,
+    },
+    {
+      title: "Straight Talk, Always",
+      description: "No games. No bullshit. We rely on our peers to improve. Be open, honest, and kind.",
+      icon: Heart,
+    },
+    {
+      title: "Pride in Every Creation",
+      description: "Passion drives excellence. High standards of quality and integrity ensure no compromises in what gets built.",
+      icon: Forward,
+    },
+    {
+      title: "Customers Come First",
+      description: "Meeting expectations isn’t enough. Understanding both spoken and unspoken needs leads to truly exceptional customer success.",
+      icon: SmileEmogi,
+    },
+  ];
+
+export default values;
