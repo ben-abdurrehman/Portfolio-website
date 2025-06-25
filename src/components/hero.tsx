dbebc5c842326690765758785558c25b77893a9b
ben-abdurrehman <abdurrehman12k6@gmail.com>
2025-06-25 12:19:51 +0500

    Refactor Hero component layout and styling; update Firebase configuration to use environment variables for improved security.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,28 +1,27 @@
 'use client';
 
 import Link from "next/link";
-import CONSTANTS from "@/constants";
 import Image from "next/image";
 
 // import '@/styles/globals.css';
 
 export default function Hero() {
   return (
-    <section className="relative flex flex-col items-center justify-start min-h-[100vh] bg-black py-20 text-white overflow-hidden px-4">
+    <section className="relative flex flex-col items-center justify-center md:justify-start min-h-[90vh] md:min-h-[100vh] bg-black py-20 text-white overflow-hidden px-4">
       {/* Intro */}
       <Image src="/lines-bg.png" alt="background-lines" className="z-10 bg-gradient-to-b from-40% from-transparent via-60% via-transparent to-black absolute w-[100vw] h-[100vh]" width={100} height={100}/>
       <Image src="/gradientoverlay.png" alt="gradientoverlay" className="z-30 grayscale-100 absolute bottom-0 w-[100vw] h-[10vh]" width={100} height={10} />
       <div className="flex flex-col">
         <div className="mt-8 text-center z-10">
           <span className="text-base md:text-2xl flex items-start justify-center lg:items-center lg:justify-center xl:gap-2 mb:1 xl:mb-2">
-            <span role="img" aria-label="Waving hand">👋</span>
+            <span className="hidden md:flex" role="img" aria-label="Waving hand">👋</span>
             <span>Hi, my name is <b>Abdur Rehman</b> and I am a freelance</span>
           </span>
         </div>
         <div className="relative mix-blend-difference flex flex-col items-center z-20">
             <h1 className="font-extrabold mix-blend-difference text-3xl md:text-7xl text-center font-inter leading-tight mb-0">
             <span className="text-white mix-blend-difference">
-               Software engineer
+               Software Engineer
             </span>
           </h1>
           {/* <h2
@@ -37,18 +36,15 @@ export default function Hero() {
             & Photographer
           </h2> */}
         </div>
-
       </div>
-
-      {/* Headline */}
       
 
       {/* Photo */}
-      <div className="absolute top-32 bottom-0 right-0 z-10 flex justify-center w-full h-full">
+      <div className="absolute top-64 bottom-0 right-0 z-10 flex justify-center w-full h-full">
         <div className="w-80 h-[600px] md:w-[400px] grayscale-100 overflow-hidden  shadow-xl">
           <div className="absolute rounded-full w-full lg:w-1/2 h-full mx-auto -z-10 inset-0 bg-[radial-gradient(circle_at_50%_50%,rgba(0,188,166,0.5),transparent_50%)]"></div> 
           <Image
-            className=" mx-auto select-none z-20 w-72 h-96 lg:w-80 lg:h-[600px]"
+            className=" mx-auto select-none z-20 w-80 h-[500px] lg:w-80 lg:h-[600px]"
             src="/bg-remove2.png"
             alt="Abdur Rehman portrait"
             
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCM17i0heMOhqb-tj6w0Mnejs7Z1CwmZcg',
-  authDomain: 'ben-portfolio-website.firebaseapp.com',
-  projectId: 'ben-portfolio-website',
-  storageBucket: 'ben-portfolio-website.firebasestorage.app',
-  messagingSenderId: '868517422068',
-  appId: '1:868517422068:web:93b4e12964cd67486f4fa1',
-  measurementId: "G-JY9MC20K4N",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
