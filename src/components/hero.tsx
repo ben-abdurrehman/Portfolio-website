175bd2fdb3370bfef98770eb7a81f2b9b8cf0c4b
ben-abdurrehman <abdurrehman12k6@gmail.com>
2025-07-03 11:56:52 +0500

    Update Hero component styling by adjusting image positioning and replacing background images for enhanced visual appeal and consistency.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   return (
     <section className="relative flex flex-col items-center justify-center md:justify-start min-h-[90vh] md:min-h-[100vh] bg-black py-20 text-white overflow-hidden px-4">
       {/* Intro */}
-      <Image src="/lines-bg.png" alt="background-lines" className="z-10 bg-gradient-to-b from-40% from-transparent via-60% via-transparent to-black absolute w-[100vw] h-[100vh]" width={100} height={100}/>
+      <Image src="/lines-bg.png" alt="background-lines" className="z-10 absolute w-[100vw] h-[100vh]" width={100} height={100}/>
       <Image src="/gradientoverlay.png" alt="gradientoverlay" className="z-30 grayscale-100 absolute bottom-0 w-[100vw] h-[10vh]" width={100} height={10} />
       <div className="flex flex-col mb-20">
         <div className="mt-8 font-poppins text-center z-10">
@@ -28,12 +28,12 @@ export default function Hero() {
       
 
       {/* Photo */}
-      <div className="absolute top-64 lg:top-30 xl:top-32 bottom-0 right-0 z-10 flex justify-center w-full h-full">
-        <div className="overflow-hidden  shadow-xl">
+      <div className="absolute top-72 lg:top-28  bottom-0 right-0 z-10 flex justify-center w-full h-full">
+        <div className="overflow-hidden shadow-xl">
           <div className="absolute rounded-full w-full lg:w-2/5  mx-auto -z-10 inset-0 bg-[radial-gradient(circle_at_50%_50%,rgba(1,182,212,0.7),transparent_50%)]"></div>
           <Image
-            className=" mx-auto select-none grayscale z-20 w-80 lg:w-2/3"
-            src="/bg-remove2.png"
+            className=" mx-auto select-none grayscale z-20 w-4/5 lg:w-2/5"
+            src="/bg-remove3.png"
             alt="Abdur Rehman portrait"
             
             style={{ objectFit: 'cover' }}
@@ -46,3 +46,5 @@ export default function Hero() {
     </section>
   );
 }
+
+
