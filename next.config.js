19c9e82dc42e1a87fb8afd837b771903987f6dcd
ben-abdurrehman <abdurrehman12k6@gmail.com>
2025-08-04 01:26:49 +0500

    Remove ESLint configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
